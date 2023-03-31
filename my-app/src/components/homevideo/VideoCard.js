d51d445e9c9e76052d97ef498b96e4e4ebad6777
hungnkb <hungnkb@gmail.com>
2023-03-31 13:47:01 +0700

    fix videocard

--- my-app/src/components/homevideo/VideoCard.js
+++ my-app/src/components/homevideo/VideoCard.js
@@ -6,6 +6,7 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../../utils/constants";
 
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
+    console.log(snippet);
     return (
         <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius:4 }} >
             <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` } >
