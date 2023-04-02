62b458d2af21058086528573969c2cfefac96fba
hungnkb <hungnkb@gmail.com>
2023-04-02 22:00:11 +0700

    s

--- my-app/src/components/navbar/MenuProfile.js
+++ my-app/src/components/navbar/MenuProfile.js
@@ -1,13 +1,100 @@
-import { Stack, Typography } from "@mui/material";
+import { Box, Divider, IconButton, ListItemIcon, Menu, Tooltip, Typography } from "@mui/material";
+import Stack from '@mui/material/Stack';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+import { useState } from "react";
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Avatar from '@mui/material/Avatar';
+import { maxWidth } from "@mui/system";
+import LogoutIcon from '@mui/icons-material/Logout';
+import { NavLink } from 'react-router-dom'
 
 export const MenuProfile = (props) => {
-    const title = props.title
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const title = props.title;
+    const avatar = props.avatar;
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
         <>
-            <Typography>
-                {title}
-            </Typography>
+            <>
+                <Stack direction='row' sx={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }} onClick={handleClick}>
+                    <Typography>{title}</Typography>
+                    <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+                        <Tooltip title="Account settings">
+                            <IconButton
+                                size="small"
+                                sx={{ ml: 2 }}
+                                aria-controls={open ? 'account-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={open ? 'true' : undefined}
+                            >
+                                <Avatar src={avatar} style={{ width: 32, height: 32 }} />
+                            </IconButton>
+                        </Tooltip>
+                    </Box>
+                </Stack>
+
+                <Menu
+                    anchorEl={anchorEl}
+                    id="account-menu"
+                    open={open}
+                    onClose={handleClose}
+                    onClick={handleClose}
+                    PaperProps={{
+                        elevation: 0,
+                        sx: {
+                            overflow: 'visible',
+                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                            mt: 1.5,
+                            '& .MuiAvatar-root': {
+                                width: 32,
+                                height: 32,
+                                ml: -0.5,
+                                mr: 1,
+                            },
+                            '&:before': {
+                                content: '""',
+                                display: 'block',
+                                position: 'absolute',
+                                top: 0,
+                                right: 14,
+                                width: 10,
+                                height: 10,
+                                bgcolor: 'background.paper',
+                                transform: 'translateY(-50%) rotate(45deg)',
+                                zIndex: 0,
+                            },
+                        },
+                    }}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                >
+                    <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                            <AccountCircleIcon fontSize="medium" sx={{ paddingRight: '15px' }} />
+                        </ListItemIcon>
+                        <NavLink to='/profile'>Profile</NavLink>
+                    </MenuItem>
+                    <Divider />
+                    <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                            <LogoutIcon fontSize="small" sx={{ paddingRight: '0px' }} />
+                        </ListItemIcon>
+                        Logout
+                    </MenuItem>
+                </Menu>
+            </>
+
         </>
     )
 }
