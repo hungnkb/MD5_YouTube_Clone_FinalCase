56edb48e8e335d703751a147c43f1dac07eebeec
hungnkb <hungnkb@gmail.com>
2023-04-02 12:04:31 +0700

    s

--- my-app/src/components/Upload.js
+++ my-app/src/components/Upload.js
@@ -13,38 +13,38 @@ export const Upload = () => {
 
     }
     const menuItems = [
-        { 2: 'Autos & Vehicles' },
-        { 1: 'Film & Animation' },
-        { 10: 'Music' },
-        { 15: 'Pets & Animals' },
-        { 17: 'Sports' },
-        { 18: 'Short Movies' },
-        { 19: 'Travel & Events' },
-        { 20: 'Gaming' },
-        { 21: 'Videoblogging' },
-        { 22: 'People & Blogs' },
-        { 23: 'Comedy' },
-        { 24: 'Entertainment' },
-        { 25: 'News & Politics' },
-        { 26: 'Howto & Style' },
-        { 27: 'Education' },
-        { 28: 'Science & Technology' },
-        { 29: 'Nonprofits & Activism' },
-        { 30: 'Movies' },
-        { 31: 'Anime / Animation' },
-        { 32: 'Action / Adventure' },
-        { 33: 'Classics' },
-        { 34: 'Comedy' },
-        { 35: 'Documentary' },
-        { 36: 'Drama' },
-        { 37: 'Family' },
-        { 38: 'Foreign' },
-        { 39: 'Horror' },
-        { 40: 'Sci - Fi / Fantasy' },
-        { 41: 'Thriller' },
-        { 42: 'Shorts' },
-        { 43: 'Shows' },
-        { 44: 'Trailers' },
+        { '2': 'Autos & Vehicles' },
+        { '1': 'Film & Animation' },
+        { "10": 'Music' },
+        { "15": 'Pets & Animals' },
+        { "17": 'Sports' },
+        { "18": 'Short Movies' },
+        { "19": 'Travel & Events' },
+        { "20": 'Gaming' },
+        { "21": 'Videoblogging' },
+        { "22": 'People & Blogs' },
+        { "23": 'Comedy' },
+        { "24": 'Entertainment' },
+        { "25": 'News & Politics' },
+        { "26": 'Howto & Style' },
+        { "27": 'Education' },
+        { "28": 'Science & Technology' },
+        { "29": 'Nonprofits & Activism' },
+        { "30": 'Movies' },
+        { "31": 'Anime / Animation' },
+        { "32": 'Action / Adventure' },
+        { "33": 'Classics' },
+        { "34": 'Comedy' },
+        { "35": 'Documentary' },
+        { "36": 'Drama' },
+        { "37": 'Family' },
+        { "38": 'Foreign' },
+        { "39": 'Horror' },
+        { "40": 'Sci - Fi / Fantasy' },
+        { "41": 'Thriller' },
+        { "42": 'Shorts' },
+        { "43": 'Shows' },
+        { "44": 'Trailers' },
     ]
 
     const handleUploadFile = async (e) => {
@@ -92,17 +92,15 @@ export const Upload = () => {
                     <Select
                         labelId="demo-simple-select-label"
                         id="demo-simple-select"
-                        value={22}
                         label="Please choose one"
                     >
-                        {/* {menuItems.map((item, index) => {
-                            return <MenuItem key={index} value={Object.keys(item)[0]}>{item[Object.keys(menuItems[index])]}</MenuItem>
-                        })} */}
-                        <></>
-                    </Select>
-                </div>
+                        {menuItems.map((item, index) => (
+                            <MenuItem key={index} value={Object.keys(item)[0]}>{item[Object.keys(menuItems[index])]}</MenuItem>
+                        ))}
+                </Select>
+            </div>
 
-            </form>
+        </form>
         </>
     )
 }
