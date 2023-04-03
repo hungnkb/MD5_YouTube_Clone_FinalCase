474812ce8ac1ce1309d7045cd79cf492815422c8
PhuocMai31 <Phuwowsc@gmail.com>
2023-04-03 08:16:59 +0700

    nothing

--- my-app/src/components/VideoCard.js
+++ my-app/src/components/VideoCard.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import { Typography, Card, CardContent, CardMedia } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
 
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
