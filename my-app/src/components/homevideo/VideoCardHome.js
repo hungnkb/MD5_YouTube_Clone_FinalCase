50d7a5b7c7682a188a92cd3cb5dbca7887591c2b
hungnkb <hungnkb@gmail.com>
2023-04-04 17:36:14 +0700

    s

--- my-app/src/components/homevideo/VideoCardHome.js
+++ my-app/src/components/homevideo/VideoCardHome.js
@@ -7,20 +7,20 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoCha
 
 const VideoCardHome = ({ video: { id: { videoId }, snippet } }) => {
     return (
-        <Card sx={{ width: { xs: '70%', sm: '300px' }, boxShadow: "none"}} >
-            <CardMedia style={{borderRadius: 4}}>
+        <Card sx={{ width: { xs: '70%', sm: '300px' }, boxShadow: "none" }} >
+            <CardMedia style={{ borderRadius: 4 }}>
                 <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY`} >
                     <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title}
-                               sx={{ width: { xs: '70%', sm: '300px' }, height: 180,borderRadius: 4  }}/>
+                        sx={{ width: { xs: '70%', sm: '300px' }, height: 180, borderRadius: 4 }} />
                 </Link>
             </CardMedia>
             <CardContent sx={{ backgroundColor: "white", height: '100px' }}>
-                <NavLink style={{textDecoration: 'none'}} to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+                <NavLink style={{ textDecoration: 'none' }} to={videoId ? `/video/${videoId}` : demoVideoUrl}>
                     <Typography variant="subtitle1" fontWeight="bold" color="black">
                         {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
                     </Typography>
                 </NavLink>
-                <NavLink style={{textDecoration: 'none'}} to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl} >
+                <NavLink style={{ textDecoration: 'none' }} to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl} >
                     <Typography variant="subtitle2" color="gray">
                         {snippet?.channelTitle || demoChannelTitle}
                         <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
